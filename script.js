58d94c00992044514ea09514d7eb09b0c2433d40
edd-onyango <eddonyango.dev@gmail.com>
2024-01-12 15:49:32 +0300

    create function to draw grid given a size

--- script.js
+++ script.js
@@ -1,33 +1,43 @@
 let grid = document.getElementById('gridContainer');
 let counter = 0;
 
-for(counter = 1; counter <= 16;  counter++){
-    const row = document.createElement('div')
-    row.setAttribute('style', 'display: flex; justify-content: center; gap: 5px;');
-    for(let counterRow = 1; counterRow <= 16; counterRow++){
-        const square = document.createElement('div');
-        square.setAttribute(
-            'style',
-            'background-color: salmon; color: white; width: 90px; height: 90px;',
-        );
-        square.setAttribute('class', 'pink-sq');
-        square.textContent = `${counterRow}`;
-        row.appendChild(square);
+let gridButton = document.getElementById('gridButton');
+gridButton.addEventListener('click', () => {
+    let gridSize = prompt('Please enter the desired grid size');
+    drawGrid(gridSize);
+}); 
+
+function drawGrid (sizeOfGrid) {
+    for(counter = 1; counter <= sizeOfGrid;  counter++){
+        const row = document.createElement('div')
+        row.setAttribute('style', 'display: flex; justify-content: center; gap: 5px;');
+        for(let counterRow = 1; counterRow <= sizeOfGrid; counterRow++){
+            const square = document.createElement('div');
+            square.setAttribute(
+                'style',
+                'background-color: salmon; color: white; width: 90px; height: 90px;',
+            );
+            square.setAttribute('class', 'pink-sq');
+            square.textContent = `${counterRow}`;
+            row.appendChild(square);
+        }
+        grid.appendChild(row);
     }
-    grid.appendChild(row);
+    
+    const pinkSquares = document.getElementsByClassName('pink-sq');
+    
+    for (const pinkSquare of pinkSquares) {
+        pinkSquare.addEventListener('mouseover', () =>{
+            pinkSquare.style.backgroundColor = 'blue';
+        });
+    
+        // pinkSquare.addEventListener('mouseout', () =>{
+        //     pinkSquare.style.backgroundColor = 'salmon';
+        // });
+    }
 }
 
-const pinkSquares = document.getElementsByClassName('pink-sq');
 
-for (const pinkSquare of pinkSquares) {
-    pinkSquare.addEventListener('mouseover', () =>{
-        pinkSquare.style.backgroundColor = 'blue';
-    });
-
-    // pinkSquare.addEventListener('mouseout', () =>{
-    //     pinkSquare.style.backgroundColor = 'salmon';
-    // });
-}
 
 
 
